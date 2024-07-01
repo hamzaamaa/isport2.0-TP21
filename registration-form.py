44afd5bbccb621cfc762766117d336ed2c17b34f
hamzaamaa <hamzaamaadach20@gmail.com>
2024-07-01 16:37:11 +0200

    Update registration-form.py

--- registration-form.py
+++ registration-form.py
@@ -1,109 +1,53 @@
 import json
 import boto3
-import uuid
-from botocore.exceptions import ClientError
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('registration-table')
-ses_client = boto3.client('ses', region_name='eu-central-1')
-
-def send_verification_email(email, token):
-    SENDER = "isport2.0@outlook.de"
-    RECIPIENT = email
-    SUBJECT = "Please verify your email address"
-    VERIFICATION_LINK = f"https://yb8ev7e9hh.execute-api.eu-central-1.amazonaws.com/prod/verify?token={token}"
-    BODY_TEXT = f"Please verify your email by clicking on the following link: {VERIFICATION_LINK}"
-    BODY_HTML = f"""<html>
-    <head></head>
-    <body>
-      <h1>Please verify your email address</h1>
-      <p>Please verify your email by clicking on the following link: <a href='{VERIFICATION_LINK}'>Verify Email</a></p>
-    </body>
-    </html>"""
-
-    CHARSET = "UTF-8"
-
-    try:
-        response = ses_client.send_email(
-            Destination={
-                'ToAddresses': [RECIPIENT],
-            },
-            Message={
-                'Body': {
-                    'Html': {
-                        'Charset': CHARSET,
-                        'Data': BODY_HTML,
-                    },
-                    'Text': {
-                        'Charset': CHARSET,
-                        'Data': BODY_TEXT,
-                    },
-                },
-                'Subject': {
-                    'Charset': CHARSET,
-                    'Data': SUBJECT,
-                },
-            },
-            Source=SENDER,
-        )
-        print("Email sent! Message ID:", response['MessageId'])
-    except ClientError as e:
-        print("Error sending email:", e.response['Error']['Message'])
-        raise
 
 def lambda_handler(event, context):
-    try:
-        # Get request body
-        print("Event: ", event)
+    # Get request body
+    print(event)
 
-        # Generate a unique token for email verification
-        token = str(uuid.uuid4())
+    # Get current time in UTC
+    utc_time = datetime.utcnow()
 
-        # Create new item in DynamoDB table
-        response = table.put_item(
-            Item={
-                'vorname': event['vorname'],
-                'nachname': event['nachname'],
-                'email': event['email'],
-                'geburtsdatum': event['geburtsdatum'],
-                'strasse': event['strasse'],
-                'hausnummer': event['hausnummer'],
-                'plz': event['plz'],
-                'stadt': event['stadt'],
-                'sicherheitsfrage': event['sicherheitsfrage'],
-                'sicherheitsantwort': event['sicherheitsantwort'],
-                'phone': event['phone'],
-                'password': event['password'],
-                'confirm_password': event['confirm_password'],
-                'verification_token': token,
-                'verified': False
-            }
-        )
-        print("DynamoDB put_item response:", response)
+    # Convert to local time (e.g., Europe/Berlin)
+    local_tz = ZoneInfo('Europe/Berlin')
+    local_time = utc_time.replace(tzinfo=ZoneInfo('UTC')).astimezone(local_tz)
 
-        # Send verification email
-        send_verification_email(event['email'], token)
+    # Format the local time as an ISO string
+    registrierungszeit = local_time.isoformat()
+    
 
-        # Return response
-        return {
-            'statusCode': 200,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type'
-            },
-            'body': json.dumps({'message': 'Registration successful. Please check your email to verify your account.'})
+    # Create new item in DynamoDB table
+    response = table.put_item(
+        Item={
+            'vorname': event['vorname'],
+            'nachname': event['nachname'],
+            'email': event['email'],
+            'geburtsdatum': event['geburtsdatum'],
+            'strasse': event['strasse'],
+            'hausnummer': event['hausnummer'],
+            'plz': event['plz'],
+            'stadt': event['stadt'],
+            'sicherheitsfrage': event['sicherheitsfrage'],
+            'sicherheitsantwort': event['sicherheitsantwort'],
+            'phone': event['phone'],
+            'password': event['password'],
+            'confirm_password': event['confirm_password'],
+            'registrierungszeit': registrierungszeit
         }
-    except Exception as e:
-        print("Error:", str(e))
-        return {
-            'statusCode': 500,
-            'headers': {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type'
-            },
-            'body': json.dumps({'message': 'Registration failed. Please try again later.'})
-        }
+    )
+
+    # Return response
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({'message': 'Registration successful'})
+    }
